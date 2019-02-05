0c4549b0d9c40f953589a398448a7a78d4a734e4
Kerry Imai <kc815hk@hotmail.com>
2019-02-05 00:49:13 -0500

    add test of calculation function

--- src/containers/Dashboard/calculateTwoMinAverage.spec.js
+++ src/containers/Dashboard/calculateTwoMinAverage.spec.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { shallow } from "enzyme";
+import calculateTwoMinAverage from "./calculateTwoMinAverage";
+import "babel-polyfill";
 
-it("renders test", () => {
-  console.log("test");
+it("should accurately calculate two minute average", () => {
+  expect(
+    calculateTwoMinAverage([
+      { foo: 0 },
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      { foo: 6 }
+    ])
+  ).toEqual(3);
+});
+
+it("should return -1 for short lists", () => {
+  expect(calculateTwoMinAverage([])).toEqual(-1);
+  expect(calculateTwoMinAverage([[], [], [], [], [], []])).toEqual(-1);
 });
